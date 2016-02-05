731b2845fee22279be0f3412e28cfebd35b949a2
veersuvrat <veer2812@gmail.com>
2016-02-05 09:56:43 +0000

    Completed part 1 with the basic hangman logic.

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -16,6 +16,48 @@ class HangpersonGame
     @guesses = ""
     @wrong_guesses = ""
   end
+  
+  def guess(letter)
+    if letter.nil? or letter == "" or (not letter[/[a-zA-Z]+/]  == letter)
+      raise ArgumentError
+    end
+    if @word.downcase.include? letter.downcase
+      if @guesses.include? letter.downcase
+        return false
+      else
+        @guesses.concat(letter.downcase)
+      end
+    else
+      if @wrong_guesses.include? letter.downcase
+        return false
+      else
+        @wrong_guesses.concat(letter.downcase)
+      end
+    end
+    return true
+  end
+  
+  def word_with_guesses
+    rtrn_str = ""
+    @word.split("").each do |i|
+      if @guesses.include? i
+        rtrn_str = rtrn_str + i
+      else
+        rtrn_str = rtrn_str + "-"
+      end
+    end
+    return rtrn_str
+  end
+  
+  def check_win_or_lose
+    if word_with_guesses == @word
+      return :win
+    elsif @wrong_guesses.length >= 7
+      return :lose
+    else
+      return :play
+    end
+  end
 
   def self.get_random_word
     require 'uri'
